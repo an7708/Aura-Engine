ea25835fbf894bab0c4474dd9f831225bd76249d
an7708 <sleepanddrugs@gmail.com>
2026-05-26 21:50:29 +0530

    fix: remove duplicate sku index

--- server/models/Product.model.js
+++ server/models/Product.model.js
@@ -57,7 +57,7 @@ const productSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-productSchema.index({ sku: 1 });
+//roductSchema.index({ sku: 1 });
 productSchema.index({ category: 1 });
 productSchema.index({ productName: 'text' });
 productSchema.index({ stockQuantity: 1 });
